d2a334d3458e52a7a29fc609de8fce3c2797d203
Jacob Palecki <JacobPalecki@gmail.com>
2022-10-09 20:34:38 -0700

    Second tutorial

---
+++ Avalonia.MusicStore/ViewModels/AlbumViewModel.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia.Media.Imaging;
+using Avalonia.MusicStore.Models;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.MusicStore.ViewModels
+{
+    public class AlbumViewModel : ViewModelBase
+    {
+        private Bitmap? cover;
+
+        public AlbumViewModel(Album album)
+        {
+            Album = album;
+        }
+
+        private Album Album { get; }
+
+        public string Artist => Album.Artist;
+
+        public Bitmap? Cover
+        {
+            get => cover;
+            private set => this.RaiseAndSetIfChanged(ref cover, value);
+        }
+
+        public string Title => Album.Title;
+
+        public async Task LoadCover()
+        {
+            await using (var imageStream = await Album.LoadCoverBitmapAsync())
+            {
+                Cover = await Task.Run(() => Bitmap.DecodeToWidth(imageStream, 400));
+            }
+        }
+
+        public async Task SaveToDiskAsync()
+        {
+            await Album.SaveAsync();
+
+            if (Cover != null)
+            {
+                var bitmap = Cover;
+
+                await Task.Run(() =>
+                {
+                    using (var fs = Album.SaveCoverBitmapStream())
+                    {
+                        bitmap.Save(fs);
+                    }
+                });
+            }
+        }
+
+    }
+}
--- Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
+++ Avalonia.MusicStore/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,68 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Avalonia.MusicStore.Models;
+using System.Linq;
+using System.Reactive.Concurrency;
 
 namespace Avalonia.MusicStore.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        private bool collectionEmpty;
+
+        public MainWindowViewModel()
+        {
+            ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel?>();
+
+            BuyMusicCommand = ReactiveCommand.Create(async () =>
+            {
+                var store = new MusicStoreViewModel();
+
+                var result = await ShowDialog.Handle(store);
+
+                if (result != null)
+                {
+                    Albums.Add(result);
+
+                    await result.SaveToDiskAsync();
+                }
+            });
+
+            this.WhenAnyValue(x => x.Albums.Count)
+                .Subscribe(x => CollectionEmpty = x == 0);
+
+            RxApp.MainThreadScheduler.Schedule(LoadAlbums);
+        }
+
+        public ObservableCollection<AlbumViewModel> Albums { get; } = new();
+
+        public ICommand BuyMusicCommand { get; }
+
+        public bool CollectionEmpty
+        {
+            get => collectionEmpty;
+            set => this.RaiseAndSetIfChanged(ref collectionEmpty, value);
+        }
+
+        public Interaction<MusicStoreViewModel, AlbumViewModel?> ShowDialog { get; }
+
+        private async void LoadAlbums()
+        {
+            var albums = (await Album.LoadCachedAsync()).Select(x => new AlbumViewModel(x));
+
+            foreach (var album in albums)
+            {
+                Albums.Add(album);
+            }
+
+            foreach (var album in Albums.ToList())
+            {
+                await album.LoadCover();
+            }
+        }
     }
 }
