a10a1e29afe6da367a4f29686f63b4ddb6cc6565
Jacob Palecki <JacobPalecki@gmail.com>
2022-10-10 01:02:36 -0700

    Show a chart

--- userinterface/ViewModels/ProfilesViewModel.cs
+++ userinterface/ViewModels/ProfilesViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace userinterface.ViewModels
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+
+namespace userinterface.ViewModels
 {
     public class ProfilesViewModel : ViewModelBase
     {
@@ -7,5 +10,25 @@
         }
 
         public string ProfileOneTitle { get; set; } = "Profile1";
+
+        public ISeries[] Series { get; } =
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 5, 0, 5, 0, 5, 0 },
+                Fill = null,
+                GeometrySize = 0,
+                LineSmoothness = 0,
+            },
+            new LineSeries<double>
+            {
+                Values = new double[] { 7, 2, 7, 2, 7, 2 },
+                Fill = null,
+                GeometrySize = 0,
+                LineSmoothness = 1,
+            },
+        };
+
+        public string ChartTitle { get; } = "Test Chart";
     }
 }
