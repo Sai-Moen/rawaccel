b91b0aa0ea77c35635fed21768a54c1d52d42292
Sai-Moen <vanzeijtssimon@gmail.com>
2023-11-12 01:21:27 +0100

    Going to bed

--- userinterface/Models/Charts/Chart.cs
+++ userinterface/Models/Charts/Chart.cs
@@ -1,6 +1,18 @@
-﻿namespace userinterface.Models.Charts
+﻿using Avalonia;
+
+namespace userinterface.Models.Charts
 {
     public class Chart
     {
+        public string Title { get; }
+
+        public Point[] Points { get; }
+        public static string PathToPoints => "Points";
+
+        public Chart(string title)
+        {
+            Title = title;
+            Points = new Point[3] { new(1, 1), new(10, 10), new(200, 100) };
+        }
     }
 }
