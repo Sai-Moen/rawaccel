d6c921ea27d2482a7e8b7847a698a9d9b303ae5d
Jacob Palecki <JacobPalecki@gmail.com>
2024-07-14 16:29:12 -0700

    Added MAUI userinterface project

---
+++ userinterface/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace userinterface
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ userinterface/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace userinterface
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
