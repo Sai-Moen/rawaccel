d0694b76c36cb3bc8e9d73a38fa66630f42be768
Jacob Palecki <JacobPalecki@gmail.com>
2024-07-14 16:02:51 -0700

    update nuget

--- userspace-backend/Model/AccelDefinitions/Formula/SynchronousAccelerationDefinitionModel.cs
+++ userspace-backend/Model/AccelDefinitions/Formula/SynchronousAccelerationDefinitionModel.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using userspace_backend.Data.Profiles;
 using userspace_backend.Data.Profiles.Accel.Formula;
