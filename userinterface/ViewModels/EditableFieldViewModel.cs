029bed6cf75a36960adcfc3b8f9040d4d91a6885
Jacob Palecki <JacobPalecki@gmail.com>
2024-07-23 23:31:45 -0700

    editable field view

---
+++ userinterface/ViewModels/EditableFieldViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace userinterface.ViewModels
+{
+    public partial class EditableFieldViewModel : ViewModelBase
+    {
+        public string Name { get; set; }
+
+        public string ValueText { get; set; }
+    }
+}
---
+++ userinterface/Views/EditableFieldView.axaml.cs
@@ -0,0 +1,41 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using userinterface.ViewModels;
+
+namespace userinterface.Views;
+
+public partial class EditableFieldView : UserControl
+{
+    public EditableFieldView()
+    {
+        InitializeComponent();
+        this.DataContext = new EditableFieldViewModel();
+    }
+    
+    public void TextBox_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Return)
+        {
+            TopLevel.GetTopLevel(this)?.FocusManager?.ClearFocus();
+        }
+    }
+
+    public void LostFocusHandler(object sender, RoutedEventArgs args)
+    {
+    }
+
+    public static readonly StyledProperty<string> FieldNameProperty =
+        Avalonia.AvaloniaProperty.Register<EditableFieldView, string>(nameof(FieldName));
+
+    public string FieldName
+    {
+        get => GetValue(FieldNameProperty);
+        set
+        {
+            SetValue(FieldNameProperty, value);
+            textBlock.Text = value;
+        }
+    }
+}
