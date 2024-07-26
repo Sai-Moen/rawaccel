f4f0f2245412aabf55a7629dcc954a8f611fa0d0
Jacob Palecki <JacobPalecki@gmail.com>
2024-07-26 01:39:52 -0700

    LiveCharts added

---
+++ userinterface/Views/ProfileChartView.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using userinterface.ViewModels;
+
+namespace userinterface.Views;
+
+public partial class ProfileChartView : UserControl
+{
+    public ProfileChartView()
+    {
+        InitializeComponent();
+        this.DataContext = new ProfileChartViewModel();
+    }
+}
